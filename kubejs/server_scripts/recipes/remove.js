fd675d09bbd5b71d7ef6379e64e8ffc0ab0d3f81
renik <mrushvg@gmail.com>
2025-06-05 10:00:53 -0700

    EMC fix

--- kubejs/server_scripts/recipes/remove.js
+++ kubejs/server_scripts/recipes/remove.js
@@ -3,12 +3,25 @@
 //// renik
 
 //
-ServerEvents.recipes(event => {
-//
+// ServerEvents.recipes(event => {
+// //
 
-  //REMOVE
-  event.remove({id: 'toybox:toybox_recipe_2'})
+//   //REMOVE
+//   event.remove({id: 'toybox:toybox_recipe_2'})
 
-//
+// //
+// })
+// //
+
+// ProjectE.eventName(event => {
+//   event.item("item", 32) // item with emc value of 32
+//   event.item("#item/tag", 12)
+
+//   event.item("a")
+// })
+
+ProjectEEvents.setEMC(event => {
+    Ingredient.of("#forge:seeds").getStacks().forEach(element => {
+        event.setEMC(element.id, 0)// sets the absolute emc value of an item
+    });
 })
-//
