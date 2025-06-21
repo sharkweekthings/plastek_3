7b5b0ee10a1399769f65ddd9a7e58f103a9fba1f
renik <mrushvg@gmail.com>
2025-06-21 01:23:02 -0700

    customize passive talent tree

--- kubejs/server_scripts/game_rules.js
+++ kubejs/server_scripts/game_rules.js
@@ -5,12 +5,10 @@
 
 ServerEvents.loaded(event => {
   if (event.server.persistentData.gameRules) return
-  event.server.gameRules.set("keepInventory", true)
-  event.server.gameRules.set("theappearanceoftheNightmareStalker", false)
-  event.server.gameRules.set("maggotsAppearance", false)
-  event.server.gameRules.set("krampusSpawn", false)
-  event.server.gameRules.set("restlessSpiritSpawn", false)
-
-  event.server.persistentData.gameRules = false
+    event.server.gameRules.set("keepInventory", true)
+    event.server.gameRules.set("theappearanceoftheNightmareStalker", false)
+    event.server.gameRules.set("maggotsAppearance", false)
+    event.server.gameRules.set("krampusSpawn", false)
+    event.server.gameRules.set("restlessSpiritSpawn", false)
+    event.server.persistentData.gameRules = false
 })
-
